b1140f44f8215d6e1f6b958409bf447f57f8bb90
УкрТехноСервис <rvy@ukr.net>
2022-03-18 22:35:27 +0200

    init

---
+++ src/main/java/com/hub/accomodation/domain/AbstractEntity.java
@@ -0,0 +1,36 @@
+package com.hub.accomodation.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+abstract public class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+    pattern = "dd.MM.yyyy hh:mm:ss")
+    protected Date createdDate;
+
+    @LastModifiedDate
+    @Temporal(TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+    pattern = "dd.MM.yyyy hh:mm:ss")
+    protected Date lastModifiedDate;
+}
