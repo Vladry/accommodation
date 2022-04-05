b8e23d74acaed8604f90db5a38f2fbdc5e5d2abb
Vadym <tvadost2003@gmail.com>
2022-04-05 13:59:24 +0300

    mapbox api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,43 @@
 import Head from 'next/head'
-import {Button, Typography} from "@material-ui/core";
+import {Button, Container, TextField, Typography} from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
 import {decrement, increment} from "../redux/actions/sampleAction";
+import {useFetch} from "../hooks/useFetch";
+import {useEffect, useRef, useState} from "react";
 
 export default function Home() {
     const counter = useSelector(state => state.sampleData.counter)
     const dispatch = useDispatch();
+    const timer = useRef(null);
+
+    const [city, setCity] = useState("");
+
+    const [{data, loading, modifyData}, getData] = useFetch({
+        method: "GET",
+        instant: false,
+        initData: {features: []},
+        url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoidmFkeW0tdGFydGFrb3Zza3lpIiwiYSI6ImNraHo4bmt1ZzB2MGszMGx0dDNqZHdmaWUifQ.VVvuIigxHHYEJiQZlWItsQ`
+    })
+
+    useEffect(() => {
+        clearTimeout(timer.current)
+        if (city) {
+            timer.current = setTimeout(() => {
+                getData();
+            }, 800)
+        } else {
+            modifyData({features: []})
+        }
+    }, [city])
+
+    console.log(data)
+
+    const handleChange = (e) => {
+        setCity(e.target.value);
+    }
 
     return (
-        <div>
+        <Container>
             <Head>
                 <title>Home page</title>
                 <meta name="description" content="Home page"/>
@@ -18,6 +47,15 @@ export default function Home() {
                 <Button onClick={() => dispatch(decrement())}>-</Button>
                 <Button onClick={() => dispatch(increment())}>+</Button>
             </main>
-        </div>
+
+            <TextField onChange={handleChange} value={city} placeholder="Enter your city"/>
+
+            <ul>
+                {data.features.map(f => (
+                    <li>{f.place_name}</li>
+                ))}
+            </ul>
+
+        </Container>
     )
 }
