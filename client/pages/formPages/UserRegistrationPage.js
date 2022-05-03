c337821d6ceebe9ebe9419e1c2bfd3f1754d2279
УкрТехноСервис <rvy@ukr.net>
2022-05-03 21:16:16 +0300

    save

--- client/pages/formPages/UserRegistrationPage.js
+++ client/pages/formPages/UserRegistrationPage.js
@@ -4,52 +4,71 @@ import UserForm from "../../components/forms/UserForm";
 import Router from "next/router";
 import {signIn} from "next-auth/react";
 import {Box, Typography} from "@mui/material";
+import styled from "@emotion/styled";
 import {useTheme} from "@mui/material/styles";
 
 const UserRegistrationPage = () => {
 
-    const [error, setError] = useState(false);
-    const theme = useTheme();
-    const errorBox = (
-        <Box sx={{color: theme.palette.error.main, m: 'right'}}>
-            <Typography variant={'subtitle1'}>user with this email or password already exists!</Typography>
-            <Typography variant={'subtitle1'}>пользователь с таким имейлом или паролем уже существует!</Typography>
-            <Typography variant={'body1'}>Amend your data and try register again</Typography>
-            <Typography variant={'body1'}>Скорректируйте данные и повторите регистрацию</Typography>
-        </Box>
-    );
+        const [error, setError] = useState(false);
+        const theme = useTheme();
 
-    const handleSubmit = async (values) => {
-
-        try {
-            await api.post("/auth/registerFullUser", values);
-
-            setError(false);
-            const {email, password} = values;
-            await signIn('credentials', {redirect: false, email, password});
-            await Router.push("/");
-
-        } catch {
-            console.log('such user already exists! ');
-            setError(true);
-        }
+        const errorBox = (
+            <Box sx={{m: '0 auto'}}>
+                <ErrorBox theme={{color: theme.palette.error.main}}>
+                    <Typography variant={'subtitle1'}>user with this email or password already
+                        exists!<br/>
+                        Пользователь с таким имейлом или паролем уже существует!</Typography>
+                </ErrorBox>
+                <ErrorBox theme={{color: theme.palette.primary.main}}>
+                    <Typography variant={'body1'}>Amend your data and try
+                        register again<br/>
+                        Скорректируйте данные и повторите регистрацию</Typography>
+                </ErrorBox>
+            </Box>
+        );
 
 
-    };
+        const handleSubmit = async (values) => {
+
+            try {
+                await api.post("/auth/registerFullUser", values);
+
+                setError(false);
+                const {email, password} = values;
+                await signIn('credentials', {redirect: false, email, password});
+                await Router.push("/");
+
+            } catch {
+                console.log('such user already exists! ');
+                setError(true);
+            }
 
 
-return (
-    <>
-        {error && errorBox}
-
-        <h3 style={{textAlign: 'center', marginTop: '10px'}}
-        >Fill in new user details<br/>Заполните данные нового пользователя</h3>
+        };
 
 
-        <UserForm handleSubmit={handleSubmit}/>
-    </>
-);
-}
+        return (
+            <>
+
+
+                <h3 style={{textAlign: 'center', marginTop: '10px'}}
+                >Fill in new user details<br/>Заполните данные нового пользователя</h3>
+
+                {error && errorBox}
+
+                <UserForm handleSubmit={handleSubmit}/>
+            </>
+        );
+    }
 ;
 
 export default UserRegistrationPage;
+
+
+const ErrorBox = styled('p')(
+    ({theme}) => ({
+        color: theme.color,
+        margin: '10px auto',
+        textAlign: 'center'
+    })
+);
